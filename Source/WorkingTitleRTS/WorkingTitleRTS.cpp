b4d3599c42557cb5ef390173c5a4981b38269481
ochota <ochotasoft@gmail.com>
2016-05-01 12:40:27 +0200

    Uploading to master the UE4 Project RTS WorkingTitle

---
+++ Source/WorkingTitleRTS/WorkingTitleRTS.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WorkingTitleRTS.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WorkingTitleRTS, "WorkingTitleRTS" );
---
+++ Source/WorkingTitleRTS/WorkingTitleRTSGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "WorkingTitleRTSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORKINGTITLERTS_API AWorkingTitleRTSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
